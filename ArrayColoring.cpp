59f675ebd7df4d50f4b99787ace47c4a777aeb85
Sahil kumar <148351667+sahilk1012@users.noreply.github.com>
2026-08-03 10:55:26 +0530

    Add files via upload

---
+++ ArrayColoring.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            sum+=a[i];
+        }
+        if(sum%2==0) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
