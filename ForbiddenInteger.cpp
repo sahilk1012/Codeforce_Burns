760373403a3c20c58c5c35109c70013491198476
Sahil kumar <148351667+sahilk1012@users.noreply.github.com>
2026-08-10 12:41:12 +0530

    Add files via upload

---
+++ ForbiddenInteger.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k, x;
+        cin >> n >> k >> x;
+
+        if (n % 2 == 0)
+        {
+            if (x != 1) 
+            {
+                cout << "YES" << endl;
+                cout << n << endl;
+                for (int i = 0; i < n; i++)
+                {
+                    cout << "1 "; 
+                }
+                cout << endl;
+            }
+            else if (k >= 2) 
+            {
+                cout << "YES" << endl;
+                cout << n / 2 << endl;
+                for (int i = 0; i < n / 2; i++)
+                {
+                    cout << "2 ";
+                }
+                cout << endl;
+            }
+            else
+                cout << "NO" << endl;
+        }
+        else 
+        {
+            
+            if (x != 1)
+            {
+                cout << "YES" << endl;
+                cout << n << endl;
+                for (int i = 0; i < n; i++)
+                {
+                    cout << "1 ";
+                }
+                cout << endl;
+            }
+            else if (k >= 3)
+            {
+                cout << "YES" << endl;
+                int count = (n - 3) / 2; 
+                cout << count + 1 << endl; 
+                cout << "3 ";
+                for (int i = 0; i < count; i++)
+                {
+                    cout << "2 ";
+                }
+                cout << endl;
+            }
+            else
+                cout << "NO" << endl;
+        }
+    }
+}
